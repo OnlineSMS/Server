1e1daa2d929ea9cf8c06edb9f803cfda47a33c3a
DusterTheFirst <DusterTheFirst@gmial.com>
2016-12-23 21:06:59 -0500

    Add project files.

---
+++ server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+
+        public static void sysout(string msg, bool OmitDate) {
+            if (!OmitDate)
+                msg = string.Format("[{0:d} {0:t}] {1}", DateTime.Now, msg);
+            Console.WriteLine(msg);
+        }
+
+        public static void sysout(string msg) { sysout(msg, false); }
+
+    }
+}
