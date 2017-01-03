23310a1ffff039fed9e35a375100e08432d5ca1e
DusterTheFirst <DusterTheFirst@gmial.com>
2017-01-02 20:48:11 -0500

    how

--- server/server.cs
+++ server/server.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Threading;
 using System.Diagnostics;
 using Newtonsoft.Json;
+using System.Web;
 
 namespace SMOL {
     class SimpleSMSServer {
@@ -21,14 +22,44 @@ namespace SMOL {
         private string _rootDirectory;
         private HttpListener _listener;
         private int _port;
-        private Out _chat;
-        private List<String> _online;
-        private List<Message> _messages;
         string _filename = "chat.json";
+        private Out _chat;
+        private List<Message> _messages = new List<Message>();
+        private Dictionary<string, DateTime> _online = new Dictionary<string, DateTime>();
 
         public int Port {
             get { return _port; }
             private set { }
+        }
+
+        public void Refresh() {
+            List<string> online = new List<string>();
+            try {
+                foreach (KeyValuePair<string, DateTime> pair in _online.ToList()) {
+                    DateTime time = pair.Value;
+                    string name = pair.Key;
+
+                    if (time < DateTime.Now) {
+                        _online.Remove(name);
+                    } else {
+                        online.Add(name);
+                    }
+
+                }
+
+                if (_messages.Count > 100) {
+                    while(_messages.Count != 100) {
+                        _messages.RemoveAt(_messages.Count - 1);
+                        Console.WriteLine("Message List Over 100, Removing Value At {0}", _messages.Count);
+                    }
+                }
+                _chat = new Out(online, _messages);
+
+                File.WriteAllText(_filename, JsonConvert.SerializeObject(_chat));
+            } catch (Exception e) {
+                Console.WriteLine("Error In Refreshing Online Users");
+                Console.WriteLine(e.Message);
+            }
         }
 
         /// <summary>
@@ -73,10 +104,6 @@ namespace SMOL {
                 try {
                     HttpListenerContext context = _listener.GetContext();
                     Process(context);
-                    string chat = File.ReadAllText(_filename);
-                    Out _chat = JsonConvert.DeserializeObject<Out>(chat);
-                    _online = _chat.online;
-                    _messages = _chat.messages;
                 } catch (Exception ex) {
 
                 }
@@ -85,39 +112,66 @@ namespace SMOL {
 
         private void Process(HttpListenerContext context) {
             string path = context.Request.Url.AbsolutePath;
+            path = HttpUtility.UrlDecode(path);
             path = path.Substring(1);
-            Console.WriteLine(path);
 
             string[] names = path.Split('/');
             foreach (string s in names)
-                Console.Write(s + ",");
+                Console.Write(s + ", ");
             Console.WriteLine();
 
             if (names[0].Equals("in")) {
+                try {
+                    string message = names[1];
 
+                    Message messagep = JsonConvert.DeserializeObject<Message>(message);
+
+                    _messages.Add(messagep);
+
+                    Refresh();
+
+                    context.Response.SendJSON(File.ReadAllText(_filename));
+
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    context.Response.OutputStream.Flush();
+                } catch (Exception ex) {
+                    context.Response.SendMessage(ex.Message);
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                    context.Response.OutputStream.Flush();
+                }
             } else if (names[0].Equals("out")) {
                 try {
+                    string username = names[1];
+
+                    _online.Add(username, DateTime.Now.AddMinutes(1));
+
+                    //get online users
+                    Refresh();
+
                     Stream input = new FileStream(_filename, FileMode.Open);
 
                     //Adding permanent http response headers
-                    context.Response.ContentType = "application/json";
                     context.Response.ContentLength64 = input.Length;
                     context.Response.AddHeader("Date", DateTime.Now.ToString("r"));
                     context.Response.AddHeader("Last-Modified", System.IO.File.GetLastWriteTime(_filename).ToString("r"));
                     input.Close();
 
-                    context.Response.OutputStream.SendString(File.ReadAllText(_filename));
+                    context.Response.SendJSON(File.ReadAllText(_filename));
 
                     context.Response.StatusCode = (int)HttpStatusCode.OK;
                     context.Response.OutputStream.Flush();
+
                 } catch (Exception ex) {
-                    context.Response.OutputStream.SendString(ex.Message);
+                    context.Response.SendMessage(ex.Message);
                     context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
                     context.Response.OutputStream.Flush();
                 }
+            } else if (names[0].Equals("ping")) {
+                context.Response.SendMessage("Pong");
             } else {
-                context.Response.OutputStream.SendString("Please Use /in To Send Messages Or /out To Get The Last 100 Messages");
+                context.Response.SendMessage("Please Use /in To Send Messages Or /out To Get The Last 100 Messages");
 
                 context.Response.StatusCode = (int)HttpStatusCode.OK;
                 context.Response.OutputStream.Flush();
@@ -131,19 +185,41 @@ namespace SMOL {
             this._port = port;
             _serverThread = new Thread(this.Listen);
             _serverThread.Start();
+
+            string chat = File.ReadAllText(_filename);
+            Out _chat = JsonConvert.DeserializeObject<Out>(chat);
+            _messages = _chat.messages;
         }
 
 
     }
 
     static class Extentions {
-        public static void SendString(this Stream outs, String tosend) {
+        public static void SendJSON(this HttpListenerResponse outs, String tosend) {
+            outs.AddHeader("Access-Control-Allow-Origin", "*");
+            outs.ContentType = "application/json";
             byte[] bytes = System.Text.Encoding.UTF8.GetBytes(tosend);
-            outs.Write(bytes, 0, bytes.Length);
+            outs.OutputStream.Write(bytes, 0, bytes.Length);
+        }
+
+        public static void SendMessage(this HttpListenerResponse outs, String tosend) {
+            outs.AddHeader("Access-Control-Allow-Origin", "*");
+            tosend = JsonConvert.SerializeObject(new Alert(tosend));
+            outs.ContentType = "application/json";
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(tosend);
+            outs.OutputStream.Write(bytes, 0, bytes.Length);
+        }
+
+        [Obsolete("Use SendMessage() Instead", true)]
+        public static void SendText(this HttpListenerResponse outs, String tosend) {
+            outs.AddHeader("Access-Control-Allow-Origin", "*");
+            outs.ContentType = "application/json";
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(tosend);
+            outs.OutputStream.Write(bytes, 0, bytes.Length);
         }
     }
 
-    class Out {
+    public class Out {
         public Out(List<String> online, List<Message> messages) {
             this.online = online;
             this.messages = messages;
@@ -152,8 +228,8 @@ namespace SMOL {
         public List<Message> messages { get; }
     }
 
-    class Message {
-        Message(string name, string color, string text, List<String> mentioned) {
+    public class Message {
+        public Message(string name, string color, string text, List<String> mentioned) {
             this.name = name;
             this.color = color;
             this.text = text;
@@ -165,10 +241,11 @@ namespace SMOL {
         public List<String> mentioned { get; }
     }
 
-    class In {
-        In(Message message) {
+    public class Alert {
+        public Alert(string message) {
             this.message = message;
         }
-        public Message message { get; }
+        public string message { get; }
     }
+
 }
